d250741e13991f5445f7f70feaf8bb0e78f2c413
Jack Fox <jackfoxy@gmail.com>
2012-11-07 08:24:49 -0800

    demo problem with flex type cast

---
+++ DebugFlexHeapTuple/Gen.fs
@@ -0,0 +1,11 @@
+﻿namespace DebugFlexHeapTuple
+
+open FSharpx.DataStructures
+
+module Gen = 
+
+    let insertThruList l h =
+        List.fold (fun (h' : #IHeap<_,'a>) x -> h'.Insert  x  ) h l
+
+    let tuple l =
+        (insertThruList l (LeftistHeap.empty false)), l
---
+++ DebugFlexHeapTuple/Program.fs
@@ -0,0 +1,39 @@
+﻿namespace DebugFlexHeapTuple
+
+open FSharpx.DataStructures
+open Gen
+
+module console1 =
+
+    let tailOK t =
+        let f = (fun (t : #IHeap<_,'a> * 'a list) ->
+                                        let h = fst t
+                                        let l = snd t
+                                        let tl = h.Tail()
+                                        let tlHead =
+                                            if (tl.Length() > 0) then (tl.Head() = l.Item(1))
+                                            else true
+                                        ((tlHead && (tl.Length() = (l.Length - 1))))
+                                            )
+        f t
+
+    [<EntryPoint>]
+    let main argv = 
+        printfn "%A" argv
+
+        //this works
+        let l = ["a"; "b"; "c"; "d"; "e"; "f"]
+        printfn "%A" (tailOK (tuple l))
+
+
+        //my problem is I need to box tuples created with different heaps implementing the IHeap interface
+        //this is because NUnit apparently cannot use tuples with the TestCaseSource attribute
+        let x1 = box (tuple l)
+
+        //neither of the following two solutions work to cast my tuple to the interface
+        //let x2 : #IHeap<_,_> * list<'a> = unbox x1
+
+        //let x2 = unbox x1 :> #IHeap<_,_> * list<'a>
+
+        System.Console.ReadKey() |> ignore
+        0 // return an integer exit code
